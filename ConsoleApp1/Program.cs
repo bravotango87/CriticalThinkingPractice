4b1d5662c36b4daf27eab7c6f48509120ab05487
Brad Toney <bravotango87@gmail.com>
2020-04-24 09:54:46 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CriticalThinkingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+}
