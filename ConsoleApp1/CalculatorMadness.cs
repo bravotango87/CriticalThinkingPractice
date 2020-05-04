e82b096fd2e0616886e974af478de460a2b1f77a
Brad Toney <bravotango87@gmail.com>
2020-05-03 21:17:33 -0400

    solved math problem and the result of -335

--- ConsoleApp1/CalculatorMadness.cs
+++ ConsoleApp1/CalculatorMadness.cs
@@ -10,7 +10,7 @@ namespace CriticalThinkingExercise
 
         public CalculatorMadness()
         {
-            //GetSum();
+            GetSum();
         }
         public int AddTwoNumbers(int numberOne, int numberTwo)
         {
@@ -22,9 +22,9 @@ namespace CriticalThinkingExercise
         // create new method in class. pass in values.
         public void RunCalculations()
         {
-            int result1 = AddTwoNumbers(8, 40);
-            int result2 = AddTwoNumbers(200, 50);
-            int result = AddTwoNumbers(result1, result2);
+            int sum1 = AddTwoNumbers(8, 40);
+            int sum2 = AddTwoNumbers(200, 50);
+            int result = AddTwoNumbers(sum1, sum2);
             Console.WriteLine("The sum is " + result);
             
             
@@ -34,24 +34,36 @@ namespace CriticalThinkingExercise
 
         public int SubtractTwoNumbers(int numberOne, int numberTwo)
         {
-            int result = numberOne - numberTwo;
-            return result;
+            int sum = numberOne - numberTwo;
+            return sum;
         }
 
         // create method to multiply two numbers.
 
         public int MultiplyTwoNumbers(int numberOne, int NumberTwo)
         {
-            int result = numberOne * NumberTwo;
-            return result;
+            int sum = numberOne * NumberTwo;
+            return sum;
         }
 
         // create method to divide two numbers.
 
         public int DivideTwoNumbers(int numberOne, int numberTwo)
         {
-            int result = numberOne / numberTwo;
-            return result;
+            int sum = numberOne / numberTwo;
+            return sum;
+        }
+
+        public void GetSum()
+        {
+            int sum1 = AddTwoNumbers(6, 5);
+            int sum2 = MultiplyTwoNumbers(40, 35);
+            int sum3 = DivideTwoNumbers(sum2, 4);
+            int sum4 = SubtractTwoNumbers(sum1, sum3);
+            int sum5 = MultiplyTwoNumbers(2, 2);
+            int sumTotal = AddTwoNumbers(sum4, sum5);
+
+            Console.WriteLine(sumTotal);
         }
     }
 
