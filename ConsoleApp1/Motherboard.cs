cfe322bb83c69b4eea98a636ba0f7aef8f80c293
Brad Toney <bravotango87@gmail.com>
2020-05-04 14:02:10 -0400

    added constructor to the Motherboard class.

--- ConsoleApp1/Motherboard.cs
+++ ConsoleApp1/Motherboard.cs
@@ -9,21 +9,21 @@ namespace CriticalThinkingExercise
         // member methods (Has a)
 
 
-        public string Manufacturer;
-        public RAM TemporaryMemory;
-        public CPU Processor;
-        public HardDrive Storage;
-        public GPU Graphics;
+        public string manufacturer;
+        public RAM temporaryMemory;
+        public CPU processor;
+        public HardDrive storage;
+        public GPU graphics;
         
        // constructor (Spawner)
 
         public Motherboard(string manufacturer, RAM ram, CPU cpu, HardDrive hardDrive, GPU gpu)
         {
-            Manufacturer = manufacturer;
-            TemporaryMemory = ram;
-            Processor = cpu;
-            Storage = hardDrive;
-            Graphics = gpu;
+            this.manufacturer = manufacturer;
+            this.temporaryMemory = ram;
+            this.processor = cpu;
+            this.storage = hardDrive;
+            this.graphics = gpu;
         }
 
         // member methods (Can do)
