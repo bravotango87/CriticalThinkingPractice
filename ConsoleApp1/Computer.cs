658b6698b2f9bfe8151ea8c0082bc9a46fdd3e87
Brad Toney <bravotango87@gmail.com>
2020-05-04 12:29:44 -0400

    added variables to the Computer class and added to the constructor also.

--- ConsoleApp1/Computer.cs
+++ ConsoleApp1/Computer.cs
@@ -6,6 +6,31 @@ namespace CriticalThinkingExercise
 {
     class Computer
     {
+      
+        // member variables (Has a)
 
+        RAM ram;
+        CPU cpu;
+        GPU gpu;
+        HardDrive hardDrive;
+        Motherboard motherboard;
+
+
+        // constructor (Spawner)
+        public Computer()
+        {
+            ram = new RAM("short term memory");
+            cpu = new CPU("Apple", "MacBook");
+            gpu = new GPU("Intel", 32);
+            hardDrive = new HardDrive(1200, 800);
+            motherboard = new Motherboard("china made", cpu, ram, hardDrive, gpu);
+        }
+       
+
+
+           
+          
+           // member methods (Can do)
+        }
     }
 }
