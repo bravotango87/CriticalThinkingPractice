b22eb6befd59ee65ce9f140074854f163cf3b116
Brad Toney <bravotango87@gmail.com>
2020-05-03 20:46:07 -0400

    added new methods to CalculatorMadness class.

--- ConsoleApp1/CalculatorMadness.cs
+++ ConsoleApp1/CalculatorMadness.cs
@@ -12,7 +12,7 @@ namespace CriticalThinkingExercise
         {
             //GetSum();
         }
-        public int AddTwonumbers(int numberOne, int numberTwo)
+        public int AddTwoNumbers(int numberOne, int numberTwo)
         {
             int result = numberOne + numberTwo;
             return result;
@@ -22,13 +22,38 @@ namespace CriticalThinkingExercise
         // create new method in class. pass in values.
         public void RunCalculations()
         {
-            int result1 = AddTwonumbers(8, 40);
-            int result2 = AddTwonumbers(200, 50);
-            int result = AddTwonumbers(result1, result2);
+            int result1 = AddTwoNumbers(8, 40);
+            int result2 = AddTwoNumbers(200, 50);
+            int result = AddTwoNumbers(result1, result2);
             Console.WriteLine("The sum is " + result);
             
             
         }
+
+        // create method to subtract 
+
+        public int SubtractTwoNumbers(int numberOne, int numberTwo)
+        {
+            int result = numberOne - numberTwo;
+            return result;
+        }
+
+        // create method to multiply two numbers.
+
+        public int MultiplyTwoNumbers(int numberOne, int NumberTwo)
+        {
+            int result = numberOne * NumberTwo;
+            return result;
+        }
+
+        // create method to divide two numbers.
+
+        public int DivideTwoNumbers(int numberOne, int numberTwo)
+        {
+            int result = numberOne / numberTwo;
+            return result;
+        }
     }
+
 }
   
