6a448e49ed2c05c08c46b2323ad585277978fe43
Brad Toney <bravotango87@gmail.com>
2020-05-03 20:06:18 -0400

    redid method to make int parameters.

---
+++ ConsoleApp1/CalculatorMadness.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CriticalThinkingExercise
+{
+    class CalculatorMadness
+    {
+        
+
+        public CalculatorMadness()
+        {
+            GetSum();   
+        }
+    }   public int AddTwonumbers(int numberOne, int numberTwo)
+    {
+        int result = numberOne + numberTwo;
+        return result;
+    }
+}
+  
